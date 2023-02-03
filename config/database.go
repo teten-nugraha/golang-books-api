c28b1c567e398963d10ce46e4c834640ed0217fd
Teten Nugraha <teten.nugraha18@gmail.com>
2023-02-03 10:38:44 +0700

    feat : add database.go

---
+++ config/database.go
@@ -0,0 +1,29 @@
+package config
+
+import (
+	"fmt"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+	"os"
+)
+
+var Database *gorm.DB
+
+func ConnectDB() {
+	var err error
+
+	host := os.Getenv("DB_HOST")
+	username := os.Getenv("DB_USER")
+	password := os.Getenv("DB_PASSWORD")
+	dbname := os.Getenv("DB_NAME")
+	port := os.Getenv("DB_PORT")
+
+	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=disable TimeZone=Africa/Lagos", host, username, password, dbname, port)
+	Database, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
+
+	if err != nil {
+		panic(err)
+	} else {
+		fmt.Println("Successfully connected to the database")
+	}
+}
