f6c62d043e94eb655bd225cecaf150fd08da6915
Teten Nugraha <teten.nugraha18@gmail.com>
2023-02-07 08:35:31 +0700

    feat : update user.go

--- models/user.go
+++ models/user.go
@@ -1,8 +1,8 @@
 package models
 
 type User struct {
-	ID       uint64 `gorm:"primary_key:auto_increment" json:"id"`
-	Username string `gorm:"size:255;not null;unique" json:"username"`
-	Password string `gorm:"size:255;not null;unique" json:"-"`
-	Books    []Book
+	ID       uint64  `gorm:"primary_key:auto_increment" json:"id"`
+	Username string  `gorm:"size:255;not null;unique" json:"username"`
+	Password string  `gorm:"size:255;not null;unique" json:"-"`
+	Books    *[]Book `json:"books,omitempty"`
 }
